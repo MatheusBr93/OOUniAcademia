41fb7bdd826fb7fa2f2fe24cc58a4a5d8b141601
MatheusBr93 <146784410+MatheusBr93@users.noreply.github.com>
2023-11-16 13:03:33 -0300

    Delete cargo.py

--- cargo.py
+++
@@ -1,7 +0,0 @@
-class Cargo():
-    def __init__(self, nome, salario_base):
-        self.nome = nome
-        self.salario_base = salario_base
-
-    def calcular_salario_bruto(self, outros_acrescimos=0):
-        return self.salario_base + outros_acrescimos
